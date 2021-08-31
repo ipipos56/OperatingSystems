88d991e214de2f1fe3aae33f03e51a67110f1be4
ipipos56 <ipipos56@bk.ru>
2021-08-31 22:29:36 +0300

    ex1 done

--- week2/ex1.c
+++ week2/ex1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
+int main() {
+    int integerValue = INT_MAX;
+    float floatValue = FLT_MAX;
+    double doubleValue = DBL_MAX;
+
+    printf("Integer ", sizeof(int), integerValue);
+    printf("Float ", sizeof(float), floatValue);
+    printf("Double ", sizeof(double), doubleValue);
+    return 0;
+}
