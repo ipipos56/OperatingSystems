3267314b81be483659fcc42e70c967b3599718a6
ipipos56 <ipipos56@bk.ru>
2021-09-21 21:47:52 +0300

    ex1.c complete

--- week5/ex1.c
+++ week5/ex1.c
@@ -1,1 +1,15 @@
+#include <pthread.h>
+#include <stdio.h>
 
+void* functionInThread(void* param) {
+    pthread_exit(0);
+}
+
+int main () {
+    pthread_t tid;
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&tid, NULL, functionInThread, (void *) &i);
+        printf("Created thread: %d\n", i);
+        pthread_join(tid, NULL);
+    }
+}
